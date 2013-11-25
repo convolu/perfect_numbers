2700809c0d72529b9c5f307128dc6a1e12c70ba9
Constantinos Voloudakis <convolu@Cv.local>
2013-11-25 21:52:11 +0000

    Initial Commit

---
+++ perfect_numbers/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  perfect_numbers
+//
+//  Created by Constantinos Voloudakis on 25/11/2013.
+//  Copyright (c) 2013 Constantinos Voloudakis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
